8f08068ba4f2387c0402d1faee90d3f1255724f4
Rosa Yudith Bustamante Vasquez <bustamantevasquezyudith@gmail.com>
2024-11-14 12:52:54 -0500

    Grupo

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -2,5 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es Rosa Yudith");
 Console.WriteLine("Mis apellidos son Bustamante Vasquez");
-
 Console.WriteLine("Me gusta escuchar musica");
+
+Console.WriteLine("Hola Grupo");
